d5ae82e619006f8bb12fa3fc4617220fabda076b
Pyae Phyo Kyaw <59681592+ppk-1996@users.noreply.github.com>
2021-11-02 17:33:07 +0630

    fix: lint error

--- src/components/Hello.spec.tsx
+++ src/components/Hello.spec.tsx
@@ -1,9 +1,8 @@
-import {render, screen} from "@testing-library/react"
-import { Hello } from "./Hello"
+import { render, screen } from "@testing-library/react";
+import { Hello } from "./Hello";
 
-it('renders "Hello World"',()=>{
-  render(<Hello/>)
+it('renders "Hello World"', () => {
+  render(<Hello />);
   const myElement = screen.getByText(/Hello World/);
-  screen.debug();
   expect(myElement).toBeInTheDocument();
-})
+});
